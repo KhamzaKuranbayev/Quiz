62d9f43ce64f4088b6297f577470bad2ac3c61e8
Khorezmiy1991 <khamza@inbox.ru>
2020-07-03 20:26:21 +0500

    Added router & navigation bar

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -5,23 +5,34 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuestionComponent } from './question.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
-import { MatCardModule } from '@angular/material/card';
-import { MatListModule } from '@angular/material/list';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { ApiService } from './api.service';
 import { QuestionsComponent } from './questions/questions.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
+const routes = [
+  {path: 'question', component: QuestionComponent},
+  {path: 'questions', component: QuestionsComponent},
+  {path: '', component: HomeComponent}
+];
 
 
 @NgModule({
   declarations: [
     AppComponent,
     QuestionComponent,
-    QuestionsComponent
+    QuestionsComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
@@ -32,7 +43,9 @@ import { QuestionsComponent } from './questions/questions.component';
     MatInputModule,
     MatCardModule,
     MatListModule,
-    FormsModule
+    MatToolbarModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [ApiService],
   bootstrap: [AppComponent]
